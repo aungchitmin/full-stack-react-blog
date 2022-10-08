38e2511ec2260af44fbfda585526340e79055035
Sea Lion <aungchitmin1112000@gmail.com>
2022-10-08 12:39:52 +0630

    api&client register done

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import cors from "cors"
+import postRoutes from "./routes/posts.js";
+import usersRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
+
+app.get("/", (req,res) => {res.json("hello this is backend homepage")})
+app.use("/api/posts", postRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Backend running");
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,20 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", inputs);
+      navigate("/login")
+    } catch (error) {
+      setErr(error.response.data);
+    }
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder='username' required />
-        <input type="email" placeholder='email' required/>
-        <input type="password" placeholder='password' required/>
-        <button>Register</button>
-        <p>Something went wrong!</p>
-        <span>Do you have an account? <Link to='/login'>Login</Link></span>
+        <input
+          name="username"
+          type="text"
+          placeholder="username"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+          required
+        />
+        <button onClick={handleSubmit}>Register</button>
+        {err &&<p>{err}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;

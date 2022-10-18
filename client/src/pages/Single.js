43c2aa689629fc7b47a532e6019584c09b561ad0
Sea Lion <aungchitmin1112000@gmail.com>
2022-10-18 21:31:13 +0630

    CRUD done

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -1,61 +1,63 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Edit from "../images/edit.png";
 import Delete from "../images/delete.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+  const { currentUser } = useContext(AuthContext);
+
+  const location = useLocation();
+  const navigate = useNavigate()
+  const postID = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postID}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [postID]);
+
+  const handleClick = async () => {
+    try {
+      await axios.delete(`/posts/${postID}`);
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
             <span>John</span>
-            <p>Posted 2 days ago</p>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={"/write?edit=2"}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={"/write?edit=2"}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleClick} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
-          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
-          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
-          corporis!
-          <br /> <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
-          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
-          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
-          corporis!Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
-          possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea
-          dolorem ratione sit debitis deserunt repellendus numquam ab vel
-          perspiciatis corporis!
-          <br /> <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
-          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
-          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
-          corporis!
-          <br /> <br />
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
-          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
-          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
-          corporis!Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
-          possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea
-          dolorem ratione sit debitis deserunt repellendus numquam ab vel
-          perspiciatis corporis!
-        </p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
       <Menu />
     </div>
